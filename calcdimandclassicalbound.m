19a430464ea32dfac8d0e46bd164b60d6865fdb1
s3nt1n3lz21 <Neil Smith@143.167.241.69>
2017-07-20 17:22:29 +0100

    updated function to use data from the class classicalboundcalculator now

--- calcdimandclassicalbound.m
+++ calcdimandclassicalbound.m
@@ -1,10 +1,11 @@
 function [dimension,smax] = calcdimandclassicalbound(n,d,m,corrcoefflist)
 % CALCDIMANDCLASSICALBOUND Calculates the classical bound of a bell
 % inequality and it's dimension.
-
-[smax,detprobsgivesmax,numdetprobs] = calcclassicalbound(n,d,m,corrcoefflist);
+instance = classicalboundcalculator(n,d,m,corrcoefflist);
+[smax,detprobsgivesmax] = calcclassicalbound(instance);
 darray = zeros(1,n);
 marray = zeros(1,n);
+numdetprobs = size(detprobsgivesmax,1);
 probdistsgivesmax = zeros(numdetprobs,(d*m)^n);
 for row = 1:numdetprobs
     vars = detprobsgivesmax(row,:);
