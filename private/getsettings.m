b42e535857e561add521dd8dfebaf2bd234e6bcf
s3nt1n3lz21 <Neil Smith@143.167.241.69>
2017-07-22 17:07:43 +0100

    The old algorithm doesn't work implemented a new algorithm that should always work now

--- private/getsettings.m
+++ private/getsettings.m
@@ -3,10 +3,13 @@ function settings = getsettings(index,m,n)
     % create an array to hold these values    
     marray = zeros(1,n);
     % in general the index of a particular group of settings is given by "index = 1 + m1*(m+1)^(n-1) + m2*(m+1)^(n-2) + m3*(m+1)^(n-3)+..."    
-    marray(1) = idivide(int32(index-1),(m+1)^(n-1));
-    remainder = (index-1)/((m+1)^(n-1))-marray(1);
-    for i1 = 2:n
-        marray(i1) = round(remainder*(m+1));
-        remainder = remainder*(m+1) - marray(i1);
+    remainder = (index-1)/((m+1)^(n-1));
+    for i1 = 1:n
+        while remainder >= 1
+            marray(i1) = marray(i1) + 1;
+            remainder = remainder - 1;
+        end
+        remainder = remainder*(m+1);
     end
-    settings = marray;       
+    settings = marray;    
+    
