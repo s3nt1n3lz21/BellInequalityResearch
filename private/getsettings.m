9eae18421ee751896d9e0e19ea0c1badaf7f232a
s3nt1n3lz21 <Neil Smith@143.167.241.69>
2017-07-22 17:46:46 +0100

    completed algorithm and now works correctly

--- private/getsettings.m
+++ private/getsettings.m
@@ -2,7 +2,7 @@ function settings = getsettings(index,m,n)
 % function to calculate what the measurements settings are for the current correlator
     % create an array to hold these values    
     marray = zeros(1,n);
-    % in general the index of a particular group of settings is given by "index = 1 + m1*(m+1)^(n-1) + m2*(m+1)^(n-2) + m3*(m+1)^(n-3)+..."    
+    % in general the index of a particular group of settings is given by "index = 1 + m1*(m+1)^(n-1) + m2*(m+1)^(n-2) + m3*(m+1)^(n-3)+..." 
     remainder = (index-1)/((m+1)^(n-1));
     for i1 = 1:n
         while remainder >= 1
