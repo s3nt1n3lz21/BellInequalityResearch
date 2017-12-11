ea67e10f7a6c8038f50232305776ffd5f7fd3953
s3nt1n3lz21 <Neil Smith@10.78.206.130>
2017-12-11 16:43:24 +0000

    Renaming Files. Got rid of repeated code. Improved readability. Changed the looping to go through columns and then rows.

--- calcdimandclassicalbound.m
+++ calcdimandclassicalbound.m
@@ -1,8 +1,8 @@
-function [dimension,smax] = calcdimandclassicalbound(n,dlist,coefflist)
+function [dimension,smax] = calcdimandclassicalbound(dlist,coefflist)
 % CALCDIMANDCLASSICALBOUND Calculates the classical bound of a bell
 % inequality and it's dimension.
 
 % Create an instance of a classicalboundanddimensioncalculator
-instance = cbanddimcalc(n,dlist,coefflist);
+instance = cbanddimcalc(dlist,coefflist);
 % Calculate the dimension and classical bound
 [dimension,smax] = calc(instance);
